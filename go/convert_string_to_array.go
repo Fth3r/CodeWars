1b5fa4bcc6195809131f56c7466449de86763eeb
Fth3r <78496011+Fth3r@users.noreply.github.com>
2024-01-02 13:58:47 -0500

    Create convert_string_to_array.go

---
+++ go/convert_string_to_array.go
@@ -0,0 +1,9 @@
+// 8 kyu kata
+
+package kata
+
+import "strings"
+
+func StringToArray(str string) []string { return strings.Split(str, " ") }
+
+// a very easy one, but helpful because it made me look into the strings library
