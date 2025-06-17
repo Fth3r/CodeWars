88a6eab8bbc09eca38079338c1ba02a095c6d11d
Jake Beckman <j.michael.beckman@gmail.com>
2025-06-16 20:13:03 -0400

    Adding a more swifty solution

--- swift/leapYears.swift
+++ swift/leapYears.swift
@@ -4,3 +4,13 @@ func isLeapYear(_ year: Int) -> Bool {
   
   return false
 }
+
+
+// That was what I came to, but this is my first kata using swift as I'm trying to learn the language.
+// The more /swifty/ way to do it would probably have been as follows
+
+func isLeapYear(_ year: Int) -> Bool {
+  (year.isMultiple(of: 400)) || (year.isMultiple(of: 4) && !year.isMultiple(of: 100))
+}
+
+// But I didn't know that method existed in the STL! Live and learn...
